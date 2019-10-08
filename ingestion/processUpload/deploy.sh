b813af9de4af3ccf0d9ff596b15ea02abc2d06fc
Mark Servidio <mservidio@google.com>
2019-10-08 19:38:53 -0400

    http://b/139768807 Allocate any necessary storage and compute regionally by inferring the location of the bucket to which processUpload is triggered

---
+++ ingestion/processUpload/deploy.sh
@@ -0,0 +1,84 @@
+#!/bin/bash -eu
+#
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <REGION>" >&2
+  exit 1
+fi
+
+BUCKET_NAME=""
+
+for i in "$@"
+do
+case $i in
+    -t=*|--trigger-bucket=*)
+    BUCKET_NAME="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+        # unknown option
+        echo "Unknown option ${i}"
+    ;;
+esac
+done
+
+if [ -z "$BUCKET_NAME" ]
+then
+    echo "--trigger-bucket must be supplied"
+    exit 1
+fi
+
+BUCKET_REGION=`gsutil ls -L -b ${BUCKET_NAME} | grep "Location constraint:" | awk 'END {print tolower($3)}'`
+
+echo "Bucket name: ${BUCKET_NAME}"
+echo "Bucket region: ${BUCKET_REGION}"
+
+FUNCTION_REGION=""
+# https://cloud.google.com/functions/docs/locations
+declare -a functionRegions=("us-central1" "us-east1" "us-east4" "europe-west1" "europe-west2" "asia-east2" "asia-northeast1")
+
+for i in "${functionRegions[@]}"
+do
+    if [ "$i" == "${BUCKET_REGION}" ] ; then
+        # echo "Found $i"
+        FUNCTION_REGION=${i}
+        break
+    fi
+done
+
+if [ -z "$FUNCTION_REGION" ]
+then
+    MAIN_REGION=`echo ${BUCKET_REGION} | awk -F"-" '{print $1}'`
+    echo "Main region: ${MAIN_REGION}"
+
+    # https://cloud.google.com/storage/docs/locations
+    case ${MAIN_REGION} in
+        "northamerica"|"us"|"southamerica"|"australia"|"nam4") FUNCTION_REGION="us-central1";;
+        "europe"|"eu"|"eur4") FUNCTION_REGION="europe-west1";;
+        "asia") FUNCTION_REGION="asia-east2";;
+        *) FUNCTION_REGION="us-central1";;
+    esac    
+fi
+
+if [ -z "$FUNCTION_REGION" ]
+then
+    echo "Function region could not be determined, exiting."
+    exit 2
+else
+    echo "Function region: ${FUNCTION_REGION}"
+    gcloud functions deploy processUpload --region=${FUNCTION_REGION} --memory=256MB --source=. --runtime=nodejs8 --entry-point=processEvent --timeout=540s --trigger-bucket="${BUCKET_NAME}"
+    exit 0
+fi
