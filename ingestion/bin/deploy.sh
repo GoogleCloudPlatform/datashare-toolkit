90efefe71bcca08fa0f450a9436e7c191203e793
mservidio <mservidio@google.com>
2019-10-23 19:43:23 -0400

    formatted (#29)

--- ingestion/bin/deploy.sh
+++ ingestion/bin/deploy.sh
@@ -15,48 +15,43 @@
 # limitations under the License.
 
 if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <BUCKET_NAME>" >&2
-  exit 1
+    echo "Usage: $0 <BUCKET_NAME>" >&2
+    exit 1
 fi
 
 BUCKET_NAME=""
 
-for i in "$@"
-do
-case $i in
-    -t=*|--trigger-bucket=*)
-    BUCKET_NAME="${i#*=}"
-    shift # past argument=value
-    ;;
+for i in "$@"; do
+    case $i in
+    -t=* | --trigger-bucket=*)
+        BUCKET_NAME="${i#*=}"
+        shift # past argument=value
+        ;;
     *)
         # unknown option
         echo "Unknown option ${i}"
-    ;;
-esac
+        ;;
+    esac
 done
 
-if [ -z "$BUCKET_NAME" ]
-then
+if [ -z "$BUCKET_NAME" ]; then
     echo "--trigger-bucket must be supplied"
     exit 2
 fi
 
-if [[ $BUCKET_NAME != gs://* ]]
-then
+if [[ $BUCKET_NAME != gs://* ]]; then
     BUCKET_NAME="gs://${BUCKET_NAME}"
     echo "Updated --trigger-bucket to '${BUCKET_NAME}'"
 fi
 
-BUCKET_REGION=`gsutil ls -L -b ${BUCKET_NAME} | grep "Location constraint:" | awk 'END {print tolower($3)}'`
-if [ $? -ne 0 ] || [ -z "$BUCKET_REGION" ]
-then
+BUCKET_REGION=$(gsutil ls -L -b ${BUCKET_NAME} | grep "Location constraint:" | awk 'END {print tolower($3)}')
+if [ $? -ne 0 ] || [ -z "$BUCKET_REGION" ]; then
     echo "Failed to find bucket location"
     exit 3
 fi
 
-ENABLED_SERVICE_LIST=`gcloud services list`
-if [ $? -ne 0 ]
-then
+ENABLED_SERVICE_LIST=$(gcloud services list)
+if [ $? -ne 0 ]; then
     echo "Failed to get active services list"
     exit 4
 fi
@@ -64,8 +59,7 @@ fi
 if [[ "$ENABLED_SERVICE_LIST" != *"cloudfunctions.googleapis.com"* ]]; then
     echo "Enabling cloudfunctions.googleapis.com api"
     gcloud services enable cloudfunctions.googleapis.com
-    if [ $? -ne 0 ]
-    then
+    if [ $? -ne 0 ]; then
         echo "Failed to enable cloudfunctions.googleapis.com api"
         exit 5
     fi
@@ -77,38 +71,34 @@ echo "Bucket name: ${BUCKET_NAME}"
 echo "Bucket region: ${BUCKET_REGION}"
 
 # https://cloud.google.com/functions/docs/locations
-AVAILABLE_FUNCTION_REGIONS=`gcloud functions regions list`
-if [ $? -ne 0 ] || [ -z "$AVAILABLE_FUNCTION_REGIONS" ]
-then
+AVAILABLE_FUNCTION_REGIONS=$(gcloud functions regions list)
+if [ $? -ne 0 ] || [ -z "$AVAILABLE_FUNCTION_REGIONS" ]; then
     echo "Unable to get available functions region list"
     exit 6
 fi
 
 FUNCTION_REGION=""
-for i in $(basename -- "$AVAILABLE_FUNCTION_REGIONS" | grep -v NAME)
-do
-    if [ "$i" == "${BUCKET_REGION}" ] ; then
+for i in $(basename -- "$AVAILABLE_FUNCTION_REGIONS" | grep -v NAME); do
+    if [ "$i" == "${BUCKET_REGION}" ]; then
         FUNCTION_REGION=${i}
         break
     fi
 done
 
-if [ -z "$FUNCTION_REGION" ]
-then
-    MAIN_REGION=`echo ${BUCKET_REGION} | awk -F"-" '{print $1}'`
+if [ -z "$FUNCTION_REGION" ]; then
+    MAIN_REGION=$(echo ${BUCKET_REGION} | awk -F"-" '{print $1}')
     echo "Main region: ${MAIN_REGION}"
 
     # https://cloud.google.com/storage/docs/locations
     case ${MAIN_REGION} in
-        "northamerica"|"us"|"southamerica"|"australia"|"nam4") FUNCTION_REGION="us-central1";;
-        "europe"|"eu"|"eur4") FUNCTION_REGION="europe-west1";;
-        "asia") FUNCTION_REGION="asia-east2";;
-        *) FUNCTION_REGION="us-central1";;
-    esac    
+    "northamerica" | "us" | "southamerica" | "australia" | "nam4") FUNCTION_REGION="us-central1" ;;
+    "europe" | "eu" | "eur4") FUNCTION_REGION="europe-west1" ;;
+    "asia") FUNCTION_REGION="asia-east2" ;;
+    *) FUNCTION_REGION="us-central1" ;;
+    esac
 fi
 
-if [ -z "$FUNCTION_REGION" ]
-then
+if [ -z "$FUNCTION_REGION" ]; then
     echo "Function region could not be determined, exiting."
     exit 7
 else
--- tests/bin/run.sh
+++ tests/bin/run.sh
@@ -15,23 +15,21 @@
 # limitations under the License.
 #
 #
-# Execute a hermetic iteration of the BQDS end-to-end scenario 
+# Execute a hermetic iteration of the BQDS end-to-end scenario
 #
 # Script will use current project but will create all other
-# necessary objects from the source repository. Will require 
+# necessary objects from the source repository. Will require
 # administrator privileges for Cloud Functions, Cloud Storage
 # and BigQuery
 
-if [ "$(basename `pwd`)" != "bin" ]
-then
-	echo "Please execute the test script from $PROJECT_HOME/tests/bin"
-	exit 1
+if [ "$(basename $(pwd))" != "bin" ]; then
+    echo "Please execute the test script from $PROJECT_HOME/tests/bin"
+    exit 1
 fi
 
 SHASUMEXE="sha1sum"
-UNAME=`uname`
-if [ "$UNAME" == "darwin" ]
-then
+UNAME=$(uname)
+if [ "$UNAME" == "darwin" ]; then
     SHASUMEXE="shasum"
 fi
 
@@ -50,28 +48,27 @@ TRANSFORM=${TESTDIR}/config/last_sale.transform.sql
 FUNCTION_DIR=${BASEDIR}/ingestion/function
 QUERY="SELECT COUNT(*) AS count FROM ${DATASET}.${TABLE}"
 
-echo "### BQDS integration test starting at $(date) ###" 
+echo "### BQDS integration test starting at $(date) ###"
 
-echo "Setting project to ${PROJECT}" 
+echo "Setting project to ${PROJECT}"
 gcloud config set project ${PROJECT}
 
-echo "Creating temporary bucket ${BUCKET}" 
-gsutil mb ${BUCKET} 
+echo "Creating temporary bucket ${BUCKET}"
+gsutil mb ${BUCKET}
 
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
     echo "Could not create ${BUCKET}"
     exit 1
 fi
 
-echo "Copying schema and transform files to ${BUCKET}" 
-gsutil cp ${SCHEMA} ${BUCKET}/bqds/${TABLE}.schema.json 
-gsutil cp ${TRANSFORM} ${BUCKET}/bqds/${TABLE}.transform.sql 
+echo "Copying schema and transform files to ${BUCKET}"
+gsutil cp ${SCHEMA} ${BUCKET}/bqds/${TABLE}.schema.json
+gsutil cp ${TRANSFORM} ${BUCKET}/bqds/${TABLE}.transform.sql
 
-echo "Deploying cloud function" 
+echo "Deploying cloud function"
 cd ${FUNCTION_DIR}
 npm install
-DEPLOY_OUT=$(npm run deploy -- --trigger-bucket=${BUCKET}) 
+DEPLOY_OUT=$(npm run deploy -- --trigger-bucket=${BUCKET})
 
 FUNCTION_NAME_REGEXP="name.*functions[\/]([a-zA-Z\d-]+)"
 [[ $DEPLOY_OUT =~ $FUNCTION_NAME_REGEXP ]]
@@ -79,9 +76,8 @@ FUNCTION_NAME="${BASH_REMATCH[1]}"
 
 echo "${DEPLOY_OUT}"
 
-if [ $? -ne 0 ]
-then
-    echo "Could not deploy cloud function to ${BUCKET}";
+if [ $? -ne 0 ]; then
+    echo "Could not deploy cloud function to ${BUCKET}"
     exit 1
 fi
 
@@ -92,52 +88,47 @@ gsutil cp ${UPLOAD} ${BUCKET}/${DATASET}.${TABLE}.${RANDO}.$(basename ${UPLOAD})
 # TODO: wait some time until upload is done?
 # TODO: tail StackDriver logs to check for progress?
 
-echo "Sleeping for a minute" 
+echo "Sleeping for a minute"
 sleep 60
 
 ##### Validation steps are here
 
-echo "Checking dataset" 
+echo "Checking dataset"
 bq ls ${DATASET}
 
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
     echo "!!!!!!!!!!!!!!!!!!!!!"
-	  echo "ERROR: dataset ${DATASET} was not created!"
+    echo "ERROR: dataset ${DATASET} was not created!"
     echo "!!!!!!!!!!!!!!!!!!!!!"
     exit 1
 fi
 
 # account for header row in CSV for validating uploaded record count
-RECORD_COUNT=`echo "$(wc -l ${UPLOAD} | awk '{print $1}') - 1"| bc`
-if [ $? -ne 0 ]
-then
+RECORD_COUNT=$(echo "$(wc -l ${UPLOAD} | awk '{print $1}') - 1" | bc)
+if [ $? -ne 0 ]; then
     echo "!!!!!!!!!!!!!!!!!!!!!"
-	  echo "ERROR: COULDN'T GET UPLOADED FILE COUNT!"
+    echo "ERROR: COULDN'T GET UPLOADED FILE COUNT!"
     echo "!!!!!!!!!!!!!!!!!!!!!"
-    exit 1   
+    exit 1
 fi
 
 echo "Executing query: ${QUERY}"
-BQ_COUNT=$(echo "${QUERY}" | bq query --format=json --quiet --headless --use_legacy_sql=false| json -a .count)
-if [ $? -ne 0 ]
-then
+BQ_COUNT=$(echo "${QUERY}" | bq query --format=json --quiet --headless --use_legacy_sql=false | json -a .count)
+if [ $? -ne 0 ]; then
     echo "!!!!!!!!!!!!!!!!!!!!!"
-	  echo "ERROR: QUERY BQ FAILED!"
+    echo "ERROR: QUERY BQ FAILED!"
     echo "!!!!!!!!!!!!!!!!!!!!!"
-    exit 1   
+    exit 1
 fi
 
 echo "File has ${RECORD_COUNT} records, BigQuery has ${BQ_COUNT}"
 
-if [ ${RECORD_COUNT} -ne ${BQ_COUNT} ]
-then
+if [ ${RECORD_COUNT} -ne ${BQ_COUNT} ]; then
     echo "!!!!!!!!!!!!!!!!!!!!!"
     echo "ERROR: # OF RECORDS IN FILE != # RECORDS IN BQ!"
     echo "!!!!!!!!!!!!!!!!!!!!!"
-    exit 1   
+    exit 1
 fi
-
 
 ##### Validation steps are complete
 
@@ -149,34 +140,31 @@ echo "Validation complete"
 # TODO: (?mservidio?) add config.json and supporting files to derive an entitlement
 # TODO: (?mservidio?) propose/author automated entitlement validation
 
-##### Tear-down steps 
+##### Tear-down steps
 
-echo "Removing cloud function $FUNCTION_NAME" 
+echo "Removing cloud function $FUNCTION_NAME"
 cd ${FUNCTION_DIR}
 gcloud functions delete $FUNCTION_NAME --quiet
 
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
     echo "Could not delete live function $(basename {FUNCTION_DIR})!"
     exit 1
 fi
 
-echo "Removing transient bucket ${BUCKET}" 
-gsutil rm -r -f ${BUCKET} 
-if [ $? -ne 0 ]
-then
+echo "Removing transient bucket ${BUCKET}"
+gsutil rm -r -f ${BUCKET}
+if [ $? -ne 0 ]; then
     echo "Could not delete bucket ${BUCKET}!"
     exit 1
 fi
 
-echo "Removing transient dataset ${DATASET}" 
-bq rm -r -f ${DATASET} 
-if [ $? -ne 0 ]
-then
+echo "Removing transient dataset ${DATASET}"
+bq rm -r -f ${DATASET}
+if [ $? -ne 0 ]; then
     echo "Could not delete dataset ${DATASET}!"
     exit 1
 fi
 
-echo "### BQDS integration test ended at $(date) ###" 
+echo "### BQDS integration test ended at $(date) ###"
 
 exit 0
