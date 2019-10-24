068b939810e87e5c8a8c498688e07589f26b96dd
mservidio <mservidio@google.com>
2019-10-24 11:01:56 -0400

    Chat bot integration (#34)
    * Update run.sh
    
    * Update cloudbuild.yaml

--- tests/bin/run.sh
+++ tests/bin/run.sh
@@ -22,147 +22,176 @@
 # administrator privileges for Cloud Functions, Cloud Storage
 # and BigQuery
 
-if [ "$(basename $(pwd))" != "bin" ]; then
-    echo "Please execute the test script from $PROJECT_HOME/tests/bin"
-    exit 1
-fi
+trap send_notification EXIT
 
-SHASUMEXE="sha1sum"
-UNAME=$(uname | awk '{print tolower($0)}')
-if [ "$UNAME" == "darwin" ]; then
-    SHASUMEXE="shasum"
-fi
+main() {
+    if [ "$(basename $(pwd))" != "bin" ]; then
+        echo "Please execute the test script from $PROJECT_HOME/tests/bin"
+        exit 1
+    fi
 
-BASEDIR=../..
-TESTDIR=${BASEDIR}/tests
-DATADIR=${TESTDIR}/data
-LOG=${TESTDIR}/logs/bqds-test.log
-RANDO="$(cat /dev/urandom | head | ${SHASUMEXE} | awk '{print $1}')"
-PROJECT=$(gcloud config get-value project)
-DATASET="test_${RANDO}"
-TABLE=last_sale
-UPLOAD=${TESTDIR}/data/last_sale.csv
-BUCKET=gs://${RANDO}
-export FUNCTION_NAME="test-${RANDO}"
-SCHEMA=${TESTDIR}/config/last_sale.schema.json
-TRANSFORM=${TESTDIR}/config/last_sale.transform.sql
-FUNCTION_DIR=${BASEDIR}/ingestion/function
-QUERY="SELECT COUNT(*) AS count FROM ${DATASET}.${TABLE}"
+    SHASUMEXE="sha1sum"
+    UNAME=$(uname | awk '{print tolower($0)}')
+    if [ "$UNAME" == "darwin" ]; then
+        SHASUMEXE="shasum"
+    fi
 
-echo "### BQDS integration test starting at $(date) ###"
+    BASEDIR=../..
+    TESTDIR=${BASEDIR}/tests
+    DATADIR=${TESTDIR}/data
+    LOG=${TESTDIR}/logs/bqds-test.log
+    RANDO="$(cat /dev/urandom | head | ${SHASUMEXE} | awk '{print $1}')"
+    PROJECT=$(gcloud config get-value project)
+    DATASET="test_${RANDO}"
+    TABLE=last_sale
+    UPLOAD=${TESTDIR}/data/last_sale.csv
+    BUCKET=gs://${RANDO}
+    export FUNCTION_NAME="test-${RANDO}"
+    SCHEMA=${TESTDIR}/config/last_sale.schema.json
+    TRANSFORM=${TESTDIR}/config/last_sale.transform.sql
+    FUNCTION_DIR=${BASEDIR}/ingestion/function
+    QUERY="SELECT COUNT(*) AS count FROM ${DATASET}.${TABLE}"
 
-echo "Setting project to ${PROJECT}"
-gcloud config set project ${PROJECT}
+    echo "### BQDS integration test starting at $(date) ###"
 
-echo "Creating temporary bucket ${BUCKET}"
-gsutil mb ${BUCKET}
+    echo "Setting project to ${PROJECT}"
+    gcloud config set project ${PROJECT}
 
-echo "Creating temporary dataset ${DATASET}"
-bq mk ${DATASET}
+    echo "Creating temporary bucket ${BUCKET}"
+    gsutil mb ${BUCKET}
 
-if [ $? -ne 0 ]; then
-    echo "Could not create ${BUCKET}"
-    exit 1
-fi
+    echo "Creating temporary dataset ${DATASET}"
+    bq mk ${DATASET}
 
-echo "Copying schema and transform files to ${BUCKET}"
-gsutil cp ${SCHEMA} ${BUCKET}/bqds/${TABLE}.schema.json
-gsutil cp ${TRANSFORM} ${BUCKET}/bqds/${TABLE}.transform.sql
+    if [ $? -ne 0 ]; then
+        echo "Could not create ${BUCKET}"
+        exit 1
+    fi
 
-echo "Deploying cloud function: ${FUNCTION_NAME}"
-cd ${FUNCTION_DIR}
-npm install
-npm run deploy -- --trigger-bucket="${BUCKET}"
+    echo "Copying schema and transform files to ${BUCKET}"
+    gsutil cp ${SCHEMA} ${BUCKET}/bqds/${TABLE}.schema.json
+    gsutil cp ${TRANSFORM} ${BUCKET}/bqds/${TABLE}.transform.sql
 
-if [ $? -ne 0 ]; then
-    echo "Could not deploy cloud function to ${BUCKET}"
-    exit 1
-fi
+    echo "Deploying cloud function: ${FUNCTION_NAME}"
+    cd ${FUNCTION_DIR}
+    npm install
+    npm run deploy -- --trigger-bucket="${BUCKET}"
 
-cd ${DATADIR}
-echo "Copying testing data ${UPLOAD} to ${BUCKET}/${DATASET}.${TABLE}.${RANDO}.$(basename ${UPLOAD})"
-gsutil cp ${UPLOAD} ${BUCKET}/${DATASET}.${TABLE}.${RANDO}.$(basename ${UPLOAD})
+    if [ $? -ne 0 ]; then
+        echo "Could not deploy cloud function to ${BUCKET}"
+        exit 1
+    fi
 
-# TODO: wait some time until upload is done?
-# TODO: tail StackDriver logs to check for progress?
+    cd ${DATADIR}
+    echo "Copying testing data ${UPLOAD} to ${BUCKET}/${DATASET}.${TABLE}.${RANDO}.$(basename ${UPLOAD})"
+    gsutil cp ${UPLOAD} ${BUCKET}/${DATASET}.${TABLE}.${RANDO}.$(basename ${UPLOAD})
 
-echo "Sleeping for a minute"
-sleep 60
+    # TODO: wait some time until upload is done?
+    # TODO: tail StackDriver logs to check for progress?
 
-##### Validation steps are here
+    echo "Sleeping for a minute"
+    sleep 60
 
-echo "Checking dataset"
-bq ls ${DATASET}
+    ##### Validation steps are here
 
-if [ $? -ne 0 ]; then
-    echo "!!!!!!!!!!!!!!!!!!!!!"
-    echo "ERROR: dataset ${DATASET} was not created!"
-    echo "!!!!!!!!!!!!!!!!!!!!!"
-    exit 1
-fi
+    echo "Checking dataset"
+    bq ls ${DATASET}
 
-# account for header row in CSV for validating uploaded record count
-RECORD_COUNT=$(echo "$(wc -l ${UPLOAD} | awk '{print $1}') - 1" | bc)
-if [ $? -ne 0 ]; then
-    echo "!!!!!!!!!!!!!!!!!!!!!"
-    echo "ERROR: COULDN'T GET UPLOADED FILE COUNT!"
-    echo "!!!!!!!!!!!!!!!!!!!!!"
-    exit 1
-fi
+    if [ $? -ne 0 ]; then
+        echo "!!!!!!!!!!!!!!!!!!!!!"
+        echo "ERROR: dataset ${DATASET} was not created!"
+        echo "!!!!!!!!!!!!!!!!!!!!!"
+        exit 1
+    fi
 
-echo "Executing query: ${QUERY}"
-BQ_COUNT=$(echo "${QUERY}" | bq query --format=json --quiet --headless --use_legacy_sql=false | json -a .count)
-if [ $? -ne 0 ]; then
-    echo "!!!!!!!!!!!!!!!!!!!!!"
-    echo "ERROR: QUERY BQ FAILED!"
-    echo "!!!!!!!!!!!!!!!!!!!!!"
-    exit 1
-fi
+    # account for header row in CSV for validating uploaded record count
+    RECORD_COUNT=$(echo "$(wc -l ${UPLOAD} | awk '{print $1}') - 1" | bc)
+    if [ $? -ne 0 ]; then
+        echo "!!!!!!!!!!!!!!!!!!!!!"
+        echo "ERROR: COULDN'T GET UPLOADED FILE COUNT!"
+        echo "!!!!!!!!!!!!!!!!!!!!!"
+        exit 1
+    fi
 
-echo "File has ${RECORD_COUNT} records, BigQuery has ${BQ_COUNT}"
+    echo "Executing query: ${QUERY}"
+    BQ_COUNT=$(echo "${QUERY}" | bq query --format=json --quiet --headless --use_legacy_sql=false | json -a .count)
+    if [ $? -ne 0 ]; then
+        echo "!!!!!!!!!!!!!!!!!!!!!"
+        echo "ERROR: QUERY BQ FAILED!"
+        echo "!!!!!!!!!!!!!!!!!!!!!"
+        exit 1
+    fi
 
-if [ ${RECORD_COUNT} -ne ${BQ_COUNT} ]; then
-    echo "!!!!!!!!!!!!!!!!!!!!!"
-    echo "ERROR: # OF RECORDS IN FILE != # RECORDS IN BQ!"
-    echo "!!!!!!!!!!!!!!!!!!!!!"
-    exit 1
-fi
+    echo "File has ${RECORD_COUNT} records, BigQuery has ${BQ_COUNT}"
 
-##### Validation steps are complete
+    if [ ${RECORD_COUNT} -ne ${BQ_COUNT} ]; then
+        echo "!!!!!!!!!!!!!!!!!!!!!"
+        echo "ERROR: # OF RECORDS IN FILE != # RECORDS IN BQ!"
+        echo "!!!!!!!!!!!!!!!!!!!!!"
+        exit 1
+    fi
 
-echo "Validation complete"
+    ##### Validation steps are complete
 
-# TODO: (sferrazza) add query validation for batch_id and xform table creation
-# TODO: (sferrazza) test schema of transformed table is consistent with transform.sql
-# TODO: (sferrazza) test different user entitlements (via two svc-accounts? problem: secrets)
-# TODO: (?mservidio?) add config.json and supporting files to derive an entitlement
-# TODO: (?mservidio?) propose/author automated entitlement validation
+    echo "Validation complete"
 
-##### Tear-down steps
+    # TODO: (sferrazza) add query validation for batch_id and xform table creation
+    # TODO: (sferrazza) test schema of transformed table is consistent with transform.sql
+    # TODO: (sferrazza) test different user entitlements (via two svc-accounts? problem: secrets)
+    # TODO: (?mservidio?) add config.json and supporting files to derive an entitlement
+    # TODO: (?mservidio?) propose/author automated entitlement validation
 
-echo "Removing cloud function $FUNCTION_NAME"
-cd ${FUNCTION_DIR}
-gcloud functions delete $FUNCTION_NAME --quiet
+    ##### Tear-down steps
 
-if [ $? -ne 0 ]; then
-    echo "Could not delete live function $(basename {FUNCTION_DIR})!"
-    exit 1
-fi
+    echo "Removing cloud function $FUNCTION_NAME"
+    cd ${FUNCTION_DIR}
+    gcloud functions delete "$FUNCTION_NAME" --quiet
 
-echo "Removing transient bucket ${BUCKET}"
-gsutil rm -r -f ${BUCKET}
-if [ $? -ne 0 ]; then
-    echo "Could not delete bucket ${BUCKET}!"
-    exit 1
-fi
+    if [ $? -ne 0 ]; then
+        echo "Could not delete live function $FUNCTION_NAME!"
+        exit 1
+    fi
 
-echo "Removing transient dataset ${DATASET}"
-bq rm -r -f ${DATASET}
-if [ $? -ne 0 ]; then
-    echo "Could not delete dataset ${DATASET}!"
-    exit 1
-fi
+    echo "Removing transient bucket ${BUCKET}"
+    gsutil rm -r -f ${BUCKET}
+    if [ $? -ne 0 ]; then
+        echo "Could not delete bucket ${BUCKET}!"
+        exit 1
+    fi
 
-echo "### BQDS integration test ended at $(date) ###"
+    echo "Removing transient dataset ${DATASET}"
+    bq rm -r -f ${DATASET}
+    if [ $? -ne 0 ]; then
+        echo "Could not delete dataset ${DATASET}!"
+        exit 1
+    fi
 
-exit 0
+    echo "### BQDS integration test ended at $(date) ###"
+
+    exit 0
+}
+
+send_notification() {
+    EXIT_CODE=$?
+    echo "Exit code is ${EXIT_CODE}"
+    STATUS="succeeded"
+    if [ $? -ne 0 ]; then
+        STATUS="FAILED"
+    fi
+
+    gsutil cp gs://bqds-ci-config/integration-tests.config . || true
+    if [ ! -z "./integration-tests.config" ] && [ ! -z "$BRANCH" ] && [ ! -z "$REV" ]; then
+        source ./integration-tests.config
+        rm ./integration-tests.config
+
+        # https://cloud.google.com/cloud-build/docs/configuring-builds/substitute-variable-values
+        curl adf -X "POST" "${HANGOUTS_CHAT_WEBHOOK_URL}" \
+            -H 'Content-Type: application/json; charset=utf-8' \
+            -d $'{
+  "text": "Integration tests '"${STATUS}"'. [url: '"${CLOUD_BUILD_STATUS_URL}"', branch: '"${BRANCH}"', revision: '"${REV}"']"
+}'
+    fi
+}
+
+main "$@"
+exit
