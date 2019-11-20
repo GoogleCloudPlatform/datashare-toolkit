bb0cd9594ce2aec8af0a2452585021df34c85117
Mark Servidio <mservidio@google.com>
2019-11-20 11:36:41 -0500

    fixing unbound warning

--- tests/bin/run.sh
+++ tests/bin/run.sh
@@ -181,7 +181,7 @@ send_notification() {
     CURRENT_PATH=$(pwd)
     echo "Copying integration-tests.config to ${CURRENT_PATH}"
     gsutil cp gs://bqds-ci-config/integration-tests.config . || true
-    if [ ! -z "./integration-tests.config" ] && [ ! -z "$BRANCH" ] && [ ! -z "$REV" ]; then
+    if [ ! -z "./integration-tests.config" ] && [ ! -z "${BRANCH:-}" ] && [ ! -z "${REV:-}" ]; then
         source ./integration-tests.config
         rm ./integration-tests.config
 
