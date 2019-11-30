00cf8c28fba5c987c82d6f4a649bfa61f8861280
Salvatore Sferrazza <sferrazza@google.com>
2019-11-30 16:56:31 -0500

    bitmex example

---
+++ examples/bitmex/quotes/config/transform.sql
@@ -0,0 +1,9 @@
+-- this naturally loses precision from nanos down to micros
+-- it could potentially be saved by storing the nano portion of timestamp
+-- in a separate column.
+TIMESTAMP(SUBSTR(REPLACE(timestamp, 'D', ' '), 0, LENGTH(timestamp) - 3)) AS timestamp,
+symbol,
+bidSize,
+bidPrice,
+askPrice,
+askSize
