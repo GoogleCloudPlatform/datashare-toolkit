205bb53dd12ddb4258cfca52e465eaf34fd44cff
Mark Servidio <mservidio@google.com>
2020-03-27 19:52:08 -0400

    Fix for sockjs console errors
    https://github.com/webpack/webpack-dev-server/issues/416

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 
 module.exports = {
+  devServer: {
+    host: 'localhost'
+  },
   publicPath: process.env.BUCKET_NAME ? `/${process.env.BUCKET_NAME}/` : '/',
   chainWebpack: config => {
     if (
