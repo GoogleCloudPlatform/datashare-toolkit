fbd0d44fd580bec2be342698267efa5dadf95535
Mark Servidio <mservidio@google.com>
2020-04-07 11:58:56 -0400

    firebase auth

--- api/v1alpha/src/index.js
+++ api/v1alpha/src/index.js
@@ -23,6 +23,8 @@ const cors = require('cors')
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
+const { checkIfAuthenticated } = require('./lib/fb/auth');
+
 const apiVersion = "v1alpha";
 const PORT = process.env.PORT || 5555;
 /************************************************************
@@ -108,6 +110,13 @@ var routes = [];
 
 // CORS will be controlled by the API GW layer
 router.all('*', cors());
+
+if (
+    process.env.NODE_ENV === 'production' ||
+    process.env.VUE_APP_APICLIENT == 'server'
+) {
+    router.all('*', checkIfAuthenticated);
+}
 
 /**
  * @swagger
---
+++ api/v1alpha/src/lib/fb/auth.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const { admin } = require('./firebase-service');
+
+const getAuthToken = (req, res, next) => {
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.split(' ')[0] === 'Bearer'
+    ) {
+        req.authToken = req.headers.authorization.split(' ')[1];
+    } else {
+        req.authToken = null;
+    }
+    next();
+};
+
+const checkIfAuthenticated = (req, res, next) => {
+    // Ignore the docs path
+    if (req.path.startsWith('/docs/')) {
+        return next();
+    }
+    getAuthToken(req, res, async () => {
+        try {
+            const { authToken } = req;
+            const userInfo = await admin
+                .auth()
+                .verifyIdToken(authToken);
+            req.authId = userInfo.uid;
+            return next();
+        } catch (e) {
+            return res
+                .status(401)
+                .send({ error: 'You are not authorized to make this request' });
+        }
+    });
+};
+
+module.exports = {
+    checkIfAuthenticated: checkIfAuthenticated
+};
