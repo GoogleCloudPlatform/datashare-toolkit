22e24322b9107e5bff401582580e2cbb4cb0a419
[USERNAME] <williamssean@google.com>
2020-06-15 17:15:42 +0000

    updated shell script to copy the Datashare UI Deployment Manager scripts to the marketplace folder.

--- marketplace/create-vm-solution-package.sh
+++ marketplace/create-vm-solution-package.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# copy api deployment sripts
+# make a backup
 cp -r vm-solution/ vm-solution-backup/
+
+# copy api deployment sripts
 cp ../api/{deploy_ds_api.py.schema,deploy_ds_api.py,deploy_ds_api.yaml} vm-solution/
 
 # copy ui deployment scripts
-#cp ../api/{deploy_ds_api.py.schema, deploy_ds_api.py.schema, deploy_ds_api.yaml} vm-solution/
+cp ../frontend/{deploy_ui_cloud_run.py.schema,deploy_ui_cloud_run.py,deploy_ui_cloud_run.yaml} vm-solution/
 
 # zip the file
 zip -r cds_solution_folder.zip vm-solution/
