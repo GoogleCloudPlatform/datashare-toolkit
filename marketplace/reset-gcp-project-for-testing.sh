ee007c290b702761c6316ba7266dbe16af16d7ed
[USERNAME] <williamssean@google.com>
2020-07-10 19:07:23 +0000

    moved code that enables the apis first, since that is required before you enable a role.

--- marketplace/reset-gcp-project-for-testing.sh
+++ marketplace/reset-gcp-project-for-testing.sh
@@ -3,17 +3,6 @@
 RUNTIME_CONFIG="datashare-vm-1-startup-config"
 VARIABLE_NAME="/success/my-instance"
 
-# Remove storage.admin role from deployment manager service account (PROJECT_NUMBER@cloudservices.gserviceaccount.com)
-PROJECT_NUMBER=`gcloud projects list --filter=$(gcloud config get-value project) --format="value(PROJECT_NUMBER)"`
-gcloud projects remove-iam-policy-binding $(gcloud config get-value project) --member="serviceAccount:$PROJECT_NUMBER@cloudservices.gserviceaccount.com" --role="roles/storage.admin"
-
-# Remove IAM admin role from Cloud Build service account (@cloudbuild.gserviceaccount.com)
-gcloud projects remove-iam-policy-binding $(gcloud config get-value project) --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" --role="roles/iam.serviceAccountAdmin"
-gcloud projects remove-iam-policy-binding $(gcloud config get-value project) --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" --role="roles/run.admin"
-gcloud projects remove-iam-policy-binding $(gcloud config get-value project) --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" --role="roles/run.serviceAgent"
-gcloud projects remove-iam-policy-binding $(gcloud config get-value project) --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" --role="roles/iam.roleAdmin"
-gcloud projects remove-iam-policy-binding $(gcloud config get-value project) --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" --role="roles/iam.securityAdmin"
-
 # disable all the APIs
 echo "Disabling GCP APIs that were enabled during the VM solution deployment..."
 gcloud services disable cloudfunctions.googleapis.com --quiet
@@ -47,6 +36,20 @@ else
     echo "Cloud Resource Manager API was not disabled."
 fi
 
+gcloud services enable container.googleapis.com --quiet
+if [ $? -eq 0 ]; then
+    echo "disabled Container API successfully."
+else 
+    echo "Container API was not disabled."
+fi
+
+gcloud services enable containerregistry.googleapis.com --quiet
+if [ $? -eq 0 ]; then
+    echo "disabled Container Register API successfully."
+else 
+    echo "Container Registry API was not disabled."
+fi
+
 echo "Deleting the Variable..."
 gcloud beta runtime-config configs variables unset /success/my-instance --config-name cds-vm-1-startup-config
 if [ $? -eq 0 ]; then
@@ -62,5 +65,21 @@ if [ $? -eq 0 ]; then
 else 
     echo "Unable to delete $RUNTIME_CONFIG"
 fi
+
+# Remove storage.admin role from deployment manager service account (PROJECT_NUMBER@cloudservices.gserviceaccount.com)
+PROJECT_NUMBER=`gcloud projects list --filter=$(gcloud config get-value project) --format="value(PROJECT_NUMBER)"`
+gcloud projects remove-iam-policy-binding $(gcloud config get-value project) --member="serviceAccount:$PROJECT_NUMBER@cloudservices.gserviceaccount.com" --role="roles/storage.admin"
+
+# Remove IAM admin role from Cloud Build service account (@cloudbuild.gserviceaccount.com)
+gcloud projects remove-iam-policy-binding $(gcloud config get-value project) --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" --role="roles/iam.serviceAccountAdmin"
+gcloud projects remove-iam-policy-binding $(gcloud config get-value project) --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" --role="roles/run.admin"
+gcloud projects remove-iam-policy-binding $(gcloud config get-value project) --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" --role="roles/run.serviceAgent"
+gcloud projects remove-iam-policy-binding $(gcloud config get-value project) --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" --role="roles/iam.roleAdmin"
+gcloud projects remove-iam-policy-binding $(gcloud config get-value project) --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" --role="roles/iam.securityAdmin"
+gcloud projects remove-iam-policy-binding $(gcloud config get-value project) --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" --role="roles/container.clusterAdmin"
+gcloud projects remove-iam-policy-binding $(gcloud config get-value project) --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" --role="roles/container.viewer"
+gcloud projects remove-iam-policy-binding $(gcloud config get-value project) --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" --role="roles/container.admin"
+# Remove cloud build service account from Compute Engine service account
+gcloud projects remove-iam-policy-binding $(gcloud config get-value project) --member="serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com" --role="roles/container.admin"
 
 # gcloud beta runtime-config configs waiters create [WAITER_NAME] --config-name [CONFIG_NAME]
