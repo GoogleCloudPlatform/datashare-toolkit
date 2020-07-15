37cdd8367d85d7d1df4b89b43ddde00645645cb5
Mark Servidio <mservidio@google.com>
2020-07-15 19:08:27 -0400

    Update commerceProcurementUtil.js

--- shared/commerceProcurementUtil.js
+++ shared/commerceProcurementUtil.js
@@ -224,11 +224,6 @@ class CommerceProcurementUtil {
         const client = await this.getClient();
         try {
             const res = await client.providers.entitlements.list({
-                // The maximum number of entries that are requested. Default size is 200.
-                //pageSize: 'placeholder-value',
-                // The token for fetching the next page.
-                //pageToken: 'placeholder-value',
-                // The parent resource name.
                 parent: this.getProviderName(this.projectId),
                 filter: filter
             });
