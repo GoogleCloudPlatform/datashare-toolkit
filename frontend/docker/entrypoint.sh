9c4e439c5fc99881d36916778f3c747b880223f1
Mark Servidio <mservidio@google.com>
2020-08-26 11:48:44 -0400

    Update entrypoint.sh

--- frontend/docker/entrypoint.sh
+++ frontend/docker/entrypoint.sh
@@ -2,18 +2,19 @@
 
 ROOT_DIR=/usr/share/nginx/html
 
-IFS='='
+UNAME=$(uname | awk '{print tolower($0)}')
+IFS=$'\n'
 for name in $(printenv); do
-    # echo $name
-    if echo $name | egrep ^VUE_APP_.+; then
-        echo "condition met"
+    if echo "${name}" | egrep ^VUE_APP_.+; then
         if [ "$UNAME" == "darwin" ]; then
             # macOS
-            echo 'Running on macOS'
+            # echo 'Running on macOS'
+            value="$(echo $name | cut -d '=' -f 2-)"; echo "$value"
             # sed -i '' -E 's/("$name"):.+/\1: "new value",/g' /usr/share/nginx/html/config.json
         else
             # linux
-            echo 'Running on linux'
+            # echo 'Running on linux'
+            value="$(echo $name | cut -d '=' -f 2-)"; echo "$value"
             # sed -i -E 's/("$name"):.+/\1: "new value",/g' /usr/share/nginx/html/config.json
         fi
     fi
