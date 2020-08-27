69a292b85e5c8b8726c5177657cc0bc4b853c191
Mark Servidio <mservidio@google.com>
2020-08-27 01:42:56 -0400

    Update entrypoint.sh

--- frontend/docker/entrypoint.sh
+++ frontend/docker/entrypoint.sh
@@ -5,23 +5,14 @@ CONFIG_PATH=${ROOT_DIR}/config/config.json
 cat $CONFIG_PATH
 echo
 
-UNAME=$(uname | awk '{print tolower($0)}')
 IFS=$'\n'
 for var in $(printenv); do
     if echo "${var}" | egrep ^VUE_APP_.+; then
         key="$(echo $var | cut -d '=' -f 1)"
         value="$(echo $var | cut -d '=' -f 2-)"
         echo "Replacing key $key with value $value"
-
-        if [ "$UNAME" == "darwin" ]; then
-            # macOS
-            # echo 'Running on macOS'
-            sed -i '' -E "s~\"($key)\":.+~\"\1\": \"$value\",~g" "$CONFIG_PATH"
-        else
-            # linux
-            # echo 'Running on linux'
-            sed -i -E "s~\"($key)\":.+~\"\1\": \"$value\",~g" "$CONFIG_PATH"
-        fi
+        sed -i -E "s~\"($key)\":.+~\"\1\": \"$value\",~g" "$CONFIG_PATH"
+        # For macOS use: sed -i '' -E "s~\"($key)\":.+~\"\1\": \"$value\",~g" "$CONFIG_PATH"
     fi
 done
 
