2ae96c4523db3f1bc2ddf5c2431b9257f3e7361f
Mark Servidio <mservidio@google.com>
2020-08-26 22:25:06 -0400

    Update entrypoint.sh

--- frontend/docker/entrypoint.sh
+++ frontend/docker/entrypoint.sh
@@ -9,17 +9,17 @@ UNAME=$(uname | awk '{print tolower($0)}')
 IFS=$'\n'
 for var in $(printenv); do
     if echo "${var}" | egrep ^VUE_APP_.+; then
+        key="$(echo $var | cut -d '=' -f 1)"
+        value="$(echo $var | cut -d '=' -f 2-)"
+        echo "Replacing key $key with value $value"
+
         if [ "$UNAME" == "darwin" ]; then
             # macOS
             # echo 'Running on macOS'
-            value="$(echo $var | cut -d '=' -f 2-)"; echo "Replace value: $value"
             sed -i '' -E "s/\"($var)\":.+/\"\1\": \"$value\",/g" "$CONFIG_PATH"
         else
             # linux
             # echo 'Running on linux'
-            key="$(echo $var | cut -d '=' -f 1)"; echo "Replace key: $key"
-            value="$(echo $var | cut -d '=' -f 2-)"; echo "Replace value: $value"
-            echo "Replacing key $key with value $value"
             sed -i -E "s/\"($key)\":.+/\"\1\": \"$value\",/g" "$CONFIG_PATH"
         fi
     fi
