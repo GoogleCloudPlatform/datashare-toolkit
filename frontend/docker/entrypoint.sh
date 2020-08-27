a2dc0d0a77906ef4d2abdbe1ee72b61dfcbef8ca
Mark Servidio <mservidio@google.com>
2020-08-26 20:02:03 -0400

    Update entrypoint.sh

--- frontend/docker/entrypoint.sh
+++ frontend/docker/entrypoint.sh
@@ -2,6 +2,8 @@
 
 ROOT_DIR=/usr/share/nginx/html
 CONFIG_PATH=${ROOT_DIR}/config/config.json
+cat $CONFIG_PATH
+echo
 
 UNAME=$(uname | awk '{print tolower($0)}')
 IFS=$'\n'
@@ -10,17 +12,19 @@ for name in $(printenv); do
         if [ "$UNAME" == "darwin" ]; then
             # macOS
             # echo 'Running on macOS'
-            value="$(echo $name | cut -d '=' -f 2-)"; echo "$value"
-            sed -i '' -E 's/("$name"):.+/\1: "${value}",/g' ${CONFIG_PATH}
+            value="$(echo $name | cut -d '=' -f 2-)"; echo "Replace value: $value"
+            sed -i '' -E "s/\"($name)\":.+/\"\1\": \"$value\",/g" ${CONFIG_PATH}
         else
             # linux
             # echo 'Running on linux'
-            value="$(echo $name | cut -d '=' -f 2-)"; echo "$value"
-            sed -i -E 's/("$name"):.+/\1: "${value}",/g' ${CONFIG_PATH}
+            value="$(echo $name | cut -d '=' -f 2-)"; echo "Replace value: $value"
+            sed -i -E "s/\"($name)\":.+/\"\1\": \"$value\",/g" ${CONFIG_PATH}
         fi
     fi
 done
 
+echo
+cat $CONFIG_PATH
 echo "Setting variables completed"
 
 echo "Starting Nginx"
