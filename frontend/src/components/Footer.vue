193e97a97ab5b8a6bb35c7a428a97f8e6ccbd262
Mark Servidio <mservidio@google.com>
2020-11-18 22:47:34 -0500

    clean up

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -2,18 +2,15 @@
   <v-footer dark color="#1876D2" app>
     Cloud FSI Solutions &copy; {{ new Date().getFullYear() }}
     <v-spacer></v-spacer>
-    {{ settings.projectId }}
+    {{ projectId }}
   </v-footer>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-
+import config from './../config';
 export default {
-  computed: {
-    ...mapGetters({
-      settings: 'settings'
-    })
-  }
+  data: () => ({
+    projectId: config.projectId
+  })
 };
 </script>
