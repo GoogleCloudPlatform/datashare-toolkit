a805eb8cd7308bb395a696fe0498e348e02fa61b
mservidio <mservidio@google.com>
2021-01-08 09:05:26 -0500

    Update EditView.vue (#417)

--- frontend/src/components/EditView.vue
+++ frontend/src/components/EditView.vue
@@ -23,6 +23,7 @@
           rules="required|bigQueryTableIdRule"
         >
           <v-text-field
+            :readonly="viewData != null"
             v-model="view.name"
             :error-messages="errors"
             :counter="1024"
@@ -52,6 +53,7 @@
           rules="required"
         >
           <v-select
+            :readonly="viewData != null"
             v-model="view.datasetId"
             :items="referenceData.managedDatasets"
             item-text="datasetId"
