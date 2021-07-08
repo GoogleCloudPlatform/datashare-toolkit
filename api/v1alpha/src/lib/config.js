cd9d37f94174a450f57ed9dc17efd9bf09f4e3cd
mservidio <mservidio@google.com>
2021-07-08 17:43:34 -0400

    Update config.js

--- api/v1alpha/src/lib/config.js
+++ api/v1alpha/src/lib/config.js
@@ -71,6 +71,8 @@ config.procurementJwksUri = process.env.PROCUREMENT_JWKS_URI || 'https://www.goo
 config.procurementIssuer = process.env.PROCUREMENT_ISSUER || 'https://www.googleapis.com/robot/v1/metadata/x509/cloud-commerce-partner@system.gserviceaccount.com';
 
 config.uiBaseUrl = process.env.UI_BASE_URL || 'http://localhost:8080';
+
+// Attribute used by Pub/Sub listener
 config.projectId = process.env.PROJECT_ID;
 
 config.gcpMarketplaceTokenCookieName = 'gmt';
