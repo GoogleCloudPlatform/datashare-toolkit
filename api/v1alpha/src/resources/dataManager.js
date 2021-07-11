97186cb2f9b3e7ee7ab1f011cb44c23014337ab5
mservidio <mservidio@google.com>
2021-07-10 22:57:41 -0400

    Update dataManager.js

--- api/v1alpha/src/resources/dataManager.js
+++ api/v1alpha/src/resources/dataManager.js
@@ -103,12 +103,14 @@ async function isDataProducer(token) {
 
     let isProducer = false;
     for (const p of config.dataProducers) {
+        console.log(`p: ${p}, hd: ${domain}`);
         if (p.toLowerCase() === userid.toLowerCase()) {
             isProducer = true;
             break;
         }
         else if (p.includes('*') || p.includes('?')) {
             if (commonUtil.wildTest(p, domain)) {
+                console.log('wild test matched');
                 isProducer = true;
                 break;
             }
