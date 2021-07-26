eefc3311016c87a6680e2f4874cfc0965d0ec2ba
mservidio <mservidio@google.com>
2021-07-26 11:40:27 -0400

    Update deploy.sh

--- api/deploy.sh
+++ api/deploy.sh
@@ -107,6 +107,7 @@ kubectl get authorizationpolicy.security.istio.io -n "$NAMESPACE"
 kubectl delete authorizationpolicy.security.istio.io -n "$NAMESPACE" --all
 cat istio-manifests/1.4/authz/* | envsubst | kubectl apply -f -
 
+# TODO: Check for marketplace install flag
 gcloud run deploy "ds-listener-${PROJECT_ID}" \
   --image gcr.io/${PROJECT_ID}/ds-listener:${TAG} \
   --region=${REGION} \
