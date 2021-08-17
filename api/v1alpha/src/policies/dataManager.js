f7df825e78594ffbc11f5ded91aa96a545c52b7d
mservidio <mservidio@google.com>
2021-08-17 00:02:36 -0400

    Update dataManager.js

--- api/v1alpha/src/policies/dataManager.js
+++ api/v1alpha/src/policies/dataManager.js
@@ -76,7 +76,13 @@ async function listUserPolicies(projectId, email) {
     WHERE lower(email) = @email AND
         (ca.isDeleted IS FALSE OR ca.isDeleted IS NULL)
 )
-SELECT datasets, rowAccessTags, marketplace, isTableBased
+SELECT
+    CASE WHEN bigQueryEnabled IS TRUE THEN datasets ELSE NULL END as datasets,
+    CASE WHEN storageEnabled IS TRUE THEN buckets ELSE NULL END as buckets,
+    CASE WHEN pubsubEnabled IS TRUE THEN topics ELSE NULL END as topics,
+    CASE WHEN bigQueryEnabled IS TRUE THEN rowAccessTags ELSE NULL END as rowAccessTags,
+    CASE WHEN bigQueryEnabled IS TRUE THEN isTableBased ELSE NULL END as isTableBased,
+    marketplace
 FROM \`${table}\` cp
 JOIN currentAccount ca ON ca.policyId = cp.policyId
 WHERE
