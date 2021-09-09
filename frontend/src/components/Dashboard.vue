337d5dd58e47c4467caa520a2977e97b2c85e5e6
mservidio <mservidio@google.com>
2021-09-09 11:14:48 -0400

    Update Dashboard.vue

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -11,28 +11,25 @@
             open-delay="200"
           >
             <v-card :elevation="hover ? 16 : 2" class="mx-3 my-3" width="300">
-              <v-img
-                :src="card.src"
-                class="white--text align-end"
-                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                height="200px"
-              >
-                <v-card-title v-text="card.title"></v-card-title>
-              </v-img>
-
+              <v-icon class="my-3" size="45">{{ card.icon }}</v-icon>
+              <v-row justify="center" align="center">
+                <v-card-title
+                  v-text="card.title"
+                  justify="center"
+                  align="center"
+                ></v-card-title>
+                <v-card-subtitle
+                  v-text="card.description"
+                  class="grey--text"
+                ></v-card-subtitle>
+              </v-row>
               <v-card-actions>
+                <div class="black--text ms-4">
+                  23
+                </div>
                 <v-spacer></v-spacer>
-
-                <v-btn icon>
-                  <v-icon>mdi-heart</v-icon>
-                </v-btn>
-
-                <v-btn icon>
-                  <v-icon>mdi-bookmark</v-icon>
-                </v-btn>
-
-                <v-btn icon>
-                  <v-icon>mdi-share-variant</v-icon>
+                <v-btn icon :to="{ path: card.path }">
+                  <v-icon>{{ icons.arrowRight }} </v-icon>
                 </v-btn>
               </v-card-actions>
             </v-card>
@@ -44,7 +41,15 @@
 </template>
 
 <script>
-import { mdiDatabase, mdiViewGrid } from '@mdi/js';
+import {
+  mdiAccountMultiple,
+  mdiBadgeAccount,
+  mdiBucketOutline,
+  mdiDatabase,
+  mdiDog,
+  mdiViewGrid,
+  mdiArrowRightBoldCircleOutline
+} from '@mdi/js';
 
 export default {
   name: 'welcome',
@@ -52,38 +57,57 @@ export default {
     msg: String
   },
   data: () => ({
+    icons: {
+      arrowRight: mdiArrowRightBoldCircleOutline
+    },
     cards: [
       {
         title: 'Datasets',
         src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg',
         flex: 12,
-        icon: mdiDatabase
+        icon: mdiDatabase,
+        path: 'datasets',
+        description: 'the description goes here sdfdsf sdf  '
       },
       {
         title: 'Authorized Views',
         src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
         flex: 6,
-        icon: mdiViewGrid
+        icon: mdiViewGrid,
+        path: 'views',
+        description: 'the description goes heresdf dsf sdf '
       },
       {
         title: 'Pub/Sub Topics',
         src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
-        flex: 6
+        flex: 6,
+        icon: mdiDog,
+        path: 'topics',
+        description: 'the description goes here sdf sdf sdf '
       },
       {
         title: 'Storage Buckets',
         src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
-        flex: 6
+        flex: 6,
+        icon: mdiBucketOutline,
+        path: 'buckets',
+        description: 'the description goes here sdfd sdf  dfsdfdsf sfddf'
       },
       {
         title: 'Accounts',
         src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
-        flex: 6
+        flex: 6,
+        icon: mdiAccountMultiple,
+        path: 'accounts',
+        description: 'the description goes here sdf sdf dfd df'
       },
       {
         title: 'Policies',
         src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
-        flex: 6
+        flex: 6,
+        icon: mdiBadgeAccount,
+        path: 'policies',
+        description: 'the description goes here sdfds dsf sdf '
       }
     ]
   })
