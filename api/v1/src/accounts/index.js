3ce2e4dbaa7f50759f55ecd244110802a9208777
mservidio <mservidio@google.com>
2022-03-02 12:39:37 -0500

    Update index.js

--- api/v1/src/accounts/index.js
+++ api/v1/src/accounts/index.js
@@ -532,6 +532,7 @@ accounts.put('/accounts/:accountId', async (req, res) => {
  *       type: string
  *       required: true
  *     - in: body
+ *       name: account_deletion
  *       description: Request parameters for Account deletion
  *       schema:
  *         type: object
