cbc4f0aaf959fd0f6966e5c4a5401fb7723a6a4a
mservidio <mservidio@google.com>
2022-03-08 12:51:35 -0500

    Update cloud-functions.tf

--- terraform/cloud-functions.tf
+++ terraform/cloud-functions.tf
@@ -68,3 +68,14 @@ resource "google_cloudfunctions_function" "datashare_cloud_function" {
     ARCHIVE_FILES = "false",
   }
 }
+
+resource "null_resource" "delete_cloud_function_temp_folder" {
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+  provisioner "local-exec" {
+    command = "rm -rf ./tmp || true"
+  }
+  
+  depends_on = [google_storage_bucket_object.cloud_function_source_code]
+}
